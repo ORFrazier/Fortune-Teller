9df87b6fb18fb4cc9a412be4c97fdbabd810e2c6
ORFrazier <zores1991@live.com>
2023-09-23 09:21:26 -0400

    Update
    Cleaned up code, and almost have it working properly! It's printing lines of code instead of the random prompts. We're nearly there!

--- Fortune Teller/Fortune_Teller.py
+++ Fortune Teller/Fortune_Teller.py
@@ -4,7 +4,7 @@ import random
 import time, sys
 another_question ="yes"
 random_number = random.randint(1,9)
-def blockPrint(random_number):
+def answer(random_number):
  if random_number == 1:
   print('Yes - it will definitely happen')
  elif random_number == 2:
@@ -25,7 +25,6 @@ def blockPrint(random_number):
   print('Very doubtful')
  else: 
   print("I don't understand that question. Why don't you try asking a different way!")
-
 while another_question == "yes":
   str(input("Welcome! I am your personal fortune teller. Look into the crystal ball and enter your question: "))
 
@@ -39,31 +38,8 @@ while another_question == "yes":
   time.sleep (1)
   print("1...")
   time.sleep (1)
-  def enablePrint(random_number):
-   print(random_number)
+  print(answer)
   
-  random_number = random.randint(1,9)
- #print(random_number) <-- making sure the random generator was working
-if random_number == 1:
-  print('Yes - it will definitely happen')
-elif random_number == 2:
-  print('It is decidedly so')
-elif random_number == 3:
-  print('Without a doubt')
-elif random_number == 4:
-  print("I'm getting some interference from the other side. Try asking a different question")
-elif random_number == 5:
-  print('Try asking in a different way.')
-elif random_number == 6:
-  print('Better not tell you now. It would change your life too much.')
-elif random_number == 7:
-  print('My sources say no')
-elif random_number == 8:
-  print("The outlook isn't so good")
-elif random_number == 9:
-  print('Very doubtful')
-else: 
-  print("I don't understand that question. Why don't you try asking a different way!")
 
   another_question = str(input("Shall we beseech the spirits to answer another question? yes/no ")).lower()
   if another_question == 'no':
